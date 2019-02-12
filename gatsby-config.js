0b6f155d3642b99a9c2d91fce46e18bd82d03494
aha-oretama <sekine_y_529@msn.com>
2019-02-12 20:59:13 +0900

    Change siteMetadata.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description: 'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    title: 'aha-oretama.work',
+    description: 'おれたまのブログ。間違っていていもよい、自分がそのとき理解したもの、感じたものを素直に書いていく。',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
